891ba997dbbc8ece852fa7397a6b93250c2e8bf4
Simon Thurston <simongethurston@gmail.com>
2023-01-18 09:27:03 -0500

    initial commit

---
+++ Decrapinator.ps1
@@ -0,0 +1,18 @@
+# Uninstall metro apps from predefined list.
+
+$wares = Get-Content .\wares.txt
+
+ForEach ($ware in $wares) {
+    Get-AppXPackage -AllUsers $ware | Remove-AppxPackage
+}
+
+# Get uninstall strings for Office Click-To-Run versions
+# Thanks, timurleng on r/sysadmin
+
+$OfficeUninstallStrings = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where {$_.DisplayName -like "*Click-to-Run*"} | Select UninstallString).UninstallString
+
+ForEach ($UninstallString in $OfficeUninstallStrings) {
+    $UninstallEXE = ($UninstallString -split '"')[1]
+    $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False"
+    Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait
+}
